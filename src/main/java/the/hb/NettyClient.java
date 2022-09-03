51b3cece72fe8fe59f609da6db3b65aafa8ee284
bhan <1214599243@qq.com>
2022-09-03 16:47:49 +0800

    客户端启动流程

--- src/main/java/the/hb/NettyClient.java
+++ src/main/java/the/hb/NettyClient.java
@@ -2,11 +2,16 @@ package the.hb;
 
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.util.AttributeKey;
 import io.netty.util.concurrent.Future;
 import io.netty.util.concurrent.GenericFutureListener;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 /**
  * <p>
@@ -15,6 +20,7 @@ import io.netty.util.concurrent.GenericFutureListener;
  * <p>2022/9/3 15:24
  */
 public class NettyClient {
+    private static final int MAX_RETRY = 5;
 
     public static void main(String[] args) {
         NioEventLoopGroup workers = new NioEventLoopGroup();
@@ -23,24 +29,33 @@ public class NettyClient {
         bootStrap
                 .group(workers)
                 .channel(NioSocketChannel.class)
+                .attr(AttributeKey.newInstance("clientName"), "nettyClient")
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
+                .option(ChannelOption.SO_KEEPALIVE, true)
+                .option(ChannelOption.TCP_NODELAY, true)
                 .handler(new ChannelInitializer<SocketChannel>() {
                     protected void initChannel(SocketChannel socketChannel) throws Exception {
 
                     }
                 });
-        connect(bootStrap, "baidu.com", 80);
+        connect(bootStrap, "localhost", 8080, MAX_RETRY);
 
     }
-    public static void connect(Bootstrap bootstrap, String host, int port){
+    public static void connect(Bootstrap bootstrap,  String host, int port, int retry){
         bootstrap.connect(host, port).addListener(new GenericFutureListener<Future<? super Void>>() {
             public void operationComplete(Future<? super Void> future) throws Exception {
                 if(future.isSuccess()){
                     System.out.println("连接成功");
+                }else if(retry == 0){
+                    System.out.println("重连次数已用完，连接失败");
                 }else{
-                    System.out.println("连接失败");
+                    int order = MAX_RETRY - retry + 1;
+                    int delay = 1 << order;
+                    System.out.println(new Date() + ":连接失败，第" + order + "次重连");
+                    bootstrap.config().group().schedule(() -> connect(bootstrap, host, port, retry - 1),
+                            delay, TimeUnit.SECONDS);
                 }
             }
         });
     }
-
 }
