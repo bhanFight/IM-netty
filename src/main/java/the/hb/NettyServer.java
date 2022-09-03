01c82f963170d2c8c3cb62a8e31a016a28166478
bhan <1214599243@qq.com>
2022-09-03 15:02:42 +0800

    服务端启动demo

---
+++ src/main/java/the/hb/NettyServer.java
@@ -0,0 +1,44 @@
+package the.hb;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.util.concurrent.Future;
+import io.netty.util.concurrent.GenericFutureListener;
+
+/**
+ * <p>
+ *
+ * @author bHan        Email:1214599243@qq.com
+ * <p>2022/9/3 14:11
+ */
+public class NettyServer {
+    public static void main(String[] args) {
+        NioEventLoopGroup boss = new NioEventLoopGroup(1);
+        NioEventLoopGroup workers = new NioEventLoopGroup();
+
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+        serverBootstrap
+                .group(boss, workers)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    protected void initChannel(NioSocketChannel nioSocketChannel) throws Exception {
+                    }
+                });
+        bind(serverBootstrap, 8080);
+    }
+    public static void bind(final ServerBootstrap serverBootstrap, final int port){
+        serverBootstrap.bind(port).addListener(new GenericFutureListener<Future<? super Void>>() {
+            public void operationComplete(Future<? super Void> future) throws Exception {
+                if(future.isSuccess()){
+                    System.out.println("端口[" + port + "]绑定成功");
+                }else{
+                    System.out.println("端口[" + port + "]绑定失败");
+                    bind(serverBootstrap, port+1);
+                }
+            }
+        });
+    }
+}
